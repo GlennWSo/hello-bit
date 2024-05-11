ec8c484bbeeb142d7d0db932052bc2a97434bc16
GlennWSo <sondellglenn@gmail.com>
2024-05-11 22:20:37 +0200

    time stamps

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+use core::sync::atomic::{AtomicUsize, Ordering};
+
 use embassy_executor::Spawner;
 use embassy_nrf::gpio::{Level, Output, OutputDrive};
 use embassy_time::Timer;
@@ -10,11 +12,14 @@ use {defmt_rtt as _, panic_probe as _};
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_nrf::init(Default::default());
+
     let _col1 = Output::new(p.P0_28, Level::Low, OutputDrive::Standard);
     let mut led = Output::new(p.P0_21, Level::Low, OutputDrive::Standard);
 
+    defmt::println!("Hello, World!");
     loop {
         led.set_high();
+        defmt::println!("led: {}", led.is_set_high());
         Timer::after_millis(300).await;
         led.set_low();
         Timer::after_millis(300).await;
