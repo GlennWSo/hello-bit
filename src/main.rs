e320848a62ea6d311452d87e23249173179227fd
GlennWSo <sondellglenn@gmail.com>
2024-05-11 23:12:35 +0200

    init: tasks

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,32 @@
 
 use core::sync::atomic::{AtomicUsize, Ordering};
 
+use defmt::println;
 use embassy_executor::Spawner;
-use embassy_nrf::gpio::{Level, Output, OutputDrive};
+use embassy_nrf::gpio::{AnyPin, Level, Output, OutputDrive, Pin};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
-// use panic_halt as _;
+
+#[embassy_executor::task]
+async fn blink(drive_pin: AnyPin, drain_pin: AnyPin, ms: u64) {
+    let mut led = Output::new(drive_pin, Level::Low, OutputDrive::Standard);
+    let _col1 = Output::new(drain_pin, Level::Low, OutputDrive::Standard);
+
+    loop {
+        led.toggle();
+        println!("led: {}", led.is_set_high());
+        Timer::after_millis(ms).await;
+    }
+}
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_nrf::init(Default::default());
 
-    let _col1 = Output::new(p.P0_28, Level::Low, OutputDrive::Standard);
-    let mut led = Output::new(p.P0_21, Level::Low, OutputDrive::Standard);
-
     defmt::println!("Hello, World!");
-    loop {
-        led.set_high();
-        defmt::info!("led: {}", led.is_set_high());
-        Timer::after_millis(300).await;
-        led.set_low();
-        Timer::after_millis(300).await;
-    }
+
+    // let _col1 = Output::new(p.P0_28, Level::Low, OutputDrive::Standard);
+    spawner
+        .spawn(blink(p.P0_21.degrade(), p.P0_28.degrade(), 500))
+        .unwrap();
 }
