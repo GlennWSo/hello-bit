b59509941df0167a9968da98e67969003e873b10
GlennWSo <sondellglenn@gmail.com>
2024-05-11 14:33:43 +0200

    embassy :)

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,22 @@
 #![no_std]
 #![no_main]
 
-use core::ptr::write_volatile;
-use cortex_m::asm::{delay, nop};
-use cortex_m_rt::entry;
-use embedded_hal::digital::StatefulOutputPin;
-use embedded_hal::{delay::DelayNs, digital::OutputPin};
-use panic_rtt_target as _;
-use rtt_target::{rprintln, rtt_init_print};
+use embassy_executor::Spawner;
+use embassy_nrf::gpio::{Level, Output, OutputDrive};
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
+// use panic_halt as _;
 
-use microbit::{board::Board, hal::timer::Timer};
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_nrf::init(Default::default());
+    let _col1 = Output::new(p.P0_28, Level::Low, OutputDrive::Standard);
+    let mut led = Output::new(p.P0_21, Level::Low, OutputDrive::Standard);
 
-#[entry]
-fn main() -> ! {
-    rtt_init_print!();
-    let mut board = match Board::take() {
-        Some(board) => board,
-        None => {
-            rprintln!("board/peripherals already taken");
-            panic!();
-        }
-    };
-    let mut t0 = Timer::new(board.TIMER0);
-
-    board.display_pins.col1.set_low();
-    let mut row1 = board.display_pins.row1;
-
-    rprintln!("init board done, entering loop");
     loop {
-        row1.toggle();
-        rprintln!("led: {}", row1.is_set_high().unwrap());
-        t0.delay_ms(500);
+        led.set_high();
+        Timer::after_millis(300).await;
+        led.set_low();
+        Timer::after_millis(300).await;
     }
 }
