be38a8545a182e2428d0872ad4168ce105786178
GlennWSo <sondellglenn@gmail.com>
2024-05-11 11:39:44 +0200

    mile: working pac blinky

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,34 @@
 #![no_main]
 
 use core::ptr::write_volatile;
-
-use cortex_m::asm::nop;
+use cortex_m::asm::{delay, nop};
+use cortex_m_rt::entry;
+use nrf52833_pac::Peripherals;
 use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
-use cortex_m_rt::entry;
-
 // const PIN_CNF21= c
+
+fn wait() {
+    for _ in 0..400_000 {
+        nop();
+    }
+}
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-    rprintln!("Hello, World!");
-    loop {}
+
+    let p = Peripherals::take().expect("Failed to take ownership of peripherals");
+    p.P0.pin_cnf[21].write(|w| w.dir().output());
+    p.P0.pin_cnf[28].write(|w| w.dir().output());
+
+    let mut is_on = false;
+
+    rprintln!("init done, entering loop");
+    loop {
+        p.P0.out.write(|w| w.pin21().bit(is_on));
+        wait();
+        is_on = !is_on;
+    }
 }
