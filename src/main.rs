a2ebfc43d1b7fe48b7737f964dd96255c8b1fd62
GlennWSo <sondellglenn@gmail.com>
2024-05-14 22:30:39 +0200

    btns

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
 #![no_std]
 #![no_main]
 
-use micromath::F32Ext;
-
 use defmt::{info, println};
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
-use {defmt_rtt as _, panic_probe as _};
-
+use embassy_futures::select::{select, Either};
+use embassy_nrf::gpio::{AnyPin, Input};
 use embassy_sync::{blocking_mutex::raw::ThreadModeRawMutex, mutex::Mutex};
-
+use embassy_time::{Duration, Timer};
 use microbit_bsp::{
     display::{Brightness, Frame},
     LedMatrix, Microbit,
 };
+use micromath::F32Ext;
+use {defmt_rtt as _, panic_probe as _};
 
 type SharedFrame = Mutex<ThreadModeRawMutex, Option<Frame<5, 5>>>;
 static FRAME: SharedFrame = SharedFrame::new(None);
@@ -48,16 +47,29 @@ async fn blink(frame: &'static SharedFrame, r: usize, c: usize, ms: u64) {
     }
 }
 
+type Btn = Input<'static, AnyPin>;
+#[embassy_executor::task]
+async fn btn_log(mut a: Btn, mut b: Btn) {
+    loop {
+        match select(a.wait_for_rising_edge(), b.wait_for_rising_edge()).await {
+            Either::First(_) => println!("a rising"),
+            Either::Second(_) => println!("b rising"),
+        }
+    }
+}
+
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     defmt::println!("Hello, World!");
     let board = Microbit::default();
+
     let mut display = board.display;
     display.set_brightness(Brightness::MAX);
     let mut frame = FRAME.lock().await;
     *frame = Some(Frame::default());
-
     spawner.spawn(blinker(display, &FRAME)).unwrap();
+    spawner.spawn(btn_log(board.btn_a, board.btn_b));
+
     let gold: f32 = 1.618_034;
     for r in 0..5_i32 {
         for c in 0..5_i32 {
