7db3e7e3dfed310544d8b3c947c795ca5cf5b146
GlennWSo <sondellglenn@gmail.com>
2024-05-15 09:40:48 +0200

    restore bsp example

--- examples/bsp.rs
+++ examples/bsp.rs
@@ -4,21 +4,18 @@
 use core::ptr::write_volatile;
 use cortex_m::asm::{delay, nop};
 use cortex_m_rt::entry;
+use defmt::*;
 use embedded_hal::digital::StatefulOutputPin;
 use embedded_hal::{delay::DelayNs, digital::OutputPin};
-use panic_rtt_target as _;
-use rtt_target::{rprintln, rtt_init_print};
-
 use microbit::{board::Board, hal::timer::Timer};
+use {defmt_rtt as _, panic_probe as _};
 
 #[entry]
 fn main() -> ! {
-    rtt_init_print!();
     let mut board = match Board::take() {
         Some(board) => board,
         None => {
-            rprintln!("board/peripherals already taken");
-            panic!();
+            defmt::panic!("board/peripherals already taken");
         }
     };
     let mut t0 = Timer::new(board.TIMER0);
@@ -26,10 +23,10 @@ fn main() -> ! {
     board.display_pins.col1.set_low();
     let mut row1 = board.display_pins.row1;
 
-    rprintln!("init board done, entering loop");
+    println!("init board done, entering loop");
     loop {
         row1.toggle();
-        rprintln!("led: {}", row1.is_set_high().unwrap());
+        println!("led: {}", row1.is_set_high().unwrap());
         t0.delay_ms(500);
     }
 }
